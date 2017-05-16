008b616bf9e4daa7b604832107fde62b2588d5ed
Vladimir Varankin <v.varankin@aimprosoft.com>
2017-05-16 17:22:46 +0300

    submit

--- src/main/webapp/resources/js/departments/mainController.js
+++ src/main/webapp/resources/js/departments/mainController.js
@@ -83,12 +83,13 @@ function ListDep (config, callBack) {
         $('#id').val('');
         $("#content").empty();
 
-        var rowForm = $('<form id="depSave" action="editDepartment" method="get">');
-        var child = $('<input id="name" type="hidden" value=""/><br>');
-        child.append('<input id="id" type="hidden" name="id"  value=""/>');
-        child.append('</form>');
+        var rowForm = $('<form id="depSave"  method="post">');
+        var child = $('<div></div>');
+        child.append('<input id="name" type="text" name="name" placeholder="Enter department" pattern="[A-Za-z]{3,}" value=""/><br>')
+        child.append('<input id="id" type="hidden" name="id"  value=""/><br>');
+        child.append('<input id="butSaveDep" type="submit" value="OK">');
         rowForm.append(child);
-        rowForm.append('<input id="butSaveDep" type="submit" value="NewDepartments">');
+        rowForm.append('</form>');
         $('#content').append(rowForm);
 
 
@@ -131,26 +132,31 @@ function ListDep (config, callBack) {
         if ($(this).attr('id') === "delete") callBack('deleteDep');
     });
 
-    $("body").on("submit", "#depSave", function () {
+    $("#content").on("submit", "#depSave", function () {
+        alert('click');
         config.depID = '';
         callBack('depEdit');
     });
 }
 
 function EditDepartment(config, callBack){
+    var config = config;
     var depID = config.depID;
     var callback = callBack;
     //clear black
 
+    $("#content").empty();
 
+    var rowForm = $('<form id="depSave"  method="post">');
+    var child = $('<div></div>');
+    child.append('<input id="name" type="text" name="name" placeholder="Enter department" pattern="[A-Za-z]{3,}" value=""/><br>')
+    child.append('<input id="id" type="hidden" name="id"  value=""/><br>');
+    child.append('<input id="butSaveDep" type="button" value="OK">');
+    rowForm.append(child);
+    rowForm.append('</form>');
+    $('#content').append(rowForm);
 
-
-    if(depID == "") {
-        console.log('depID is empty');
-        $('#name').val('');
-        $('#id').val('');
-    }
-    else {
+    if($.isNumeric(depID) ) {
         $.ajax({
             url: "/editDepartment",
             type: "GET",
@@ -166,21 +172,15 @@ function EditDepartment(config, callBack){
             }
         });
     }
-
-
-    $("#content").empty();
-    var rowForm = $('<form id="depSave" action="depSave" method="post">');
-    var child = $('<input id="name" type="text" name="name" placeholder="Enter department" pattern="[A-Za-z]{3,}" value=""/><br>');
-    child.append('<input id="id" type="hidden" name="id"  value=""/>');
-    child.append('</form>');
-    rowForm.append(child);
-    rowForm.append('<input id="butSaveDep" type="submit" value="OK">');
-    $('#content').append(rowForm);
+    else {
+        console.log('depID is empty');
+        $('#name').val('');
+        $('#id').val('');
+    }
 
 
 
-
-    $("body").on("submit", "#depSave", function () {
+    $("#content").on("click", "#depSave", function ()  {
         $.ajax({
             url: '/depSave',
             type: "POST",
@@ -194,12 +194,13 @@ function EditDepartment(config, callBack){
                 console.log(xhr, resp, text);
             }
         })
-        return false;
+
     });
 
 }
 //------------------------------------
 function DeleteDep(config, callBack) {
+    var config = config;
     var depID = config.depID;
     var callback = callBack;
 
@@ -269,9 +270,290 @@ function displayDepartments() {
         });
         $('#content').append(table);
     });
+//----------------------------------- employee
+    function EmpList(config, callBack){
+        var config = config;
+        var callback = callBack;
+        var depID = config.depID;
+
+        var showEmpForm = function (depID) {
+            $("#content").empty();
+            empFormView(depID);
+            $('#empSaveForm').attr('depID', depID);//set id in form
+        }
+
+        $.ajax({
+            url: "/employeesList",
+            type: "GET",
+            dataType: 'json',
+            data: {depID: depID},
+            success: function (data) {
+                showEmpForm(depID);
+                empTable(data);
+            },
+            error: function (xhr, resp, text) {
+                console.log(xhr, resp, text);
+            }
+
+        });
+
+        var empTable = (function (data) {
+            var table = $('<table id="empTable">' + '<caption>' + '<h2>' + 'Employees' + '</h2>' + '</caption>' + '</table>');
+            var row = $('<tr></tr>');
+
+            row.append('<th>' + " id " + '</th>');
+            row.append('<th>' + " firstName " + '</th>');
+            row.append('<th>' + " secondName " + '</th>');
+            row.append('<th>' + " grade " + '</th>');
+            row.append('<th>' + " birthday " + '</th>');
+            row.append('<th>' + " eMail " + '</th>');
+
+            row.append('<th>' + "edit" + '</th>');
+            row.append('<th>' + "delete" + '</th>');
+            table.append(row);
+            $.each(data, function (k, v) {
+                $.each(v, function (key, value) {
+                    if (key == 'id') {
+                        row = $('<tr>').attr('id', value);
+                        row.append('</tr>');
+                        row.append('<td>' + value + '</td>');
+                    }
+                    if (key == 'firstName') {
+                        row.append('<td>' + value + '</td>');
+                    }
+                    if (key == 'secondName') {
+                        row.append('<td>' + value + '</td>');
+                    }
+                    if (key == 'grade') {
+                        row.append('<td>' + value + '</td>');
+                    }
+                    if (key == 'birthday') {
+                        row.append('<td>' + value + '</td>');
+                    }
+                    if (key == 'eMail') {
+                        row.append('<td>' + value + '</td>');
+                        row.append('<td id="edit">' + 'edit' + '</td>');
+                        row.append('<td id="delete">' + 'delete' + '</td>');
+                    }
+                });
+                table.append(row);
+
+            });
+            $('#content').append(table);
+        });
+
+        //table click event
+        $("body").on("click", "#empTable td", function () {
+            empID = $(this).closest('tr').attr('id');// table row ID
+            config.empID = empID;
+            if ($(this).attr('id') === "edit") {
+                callBack('editEmp');
+            }
+            if ($(this).attr('id') === "delete") {
+                callBack('deleteEmp');
+            }
+        });
+    }
+//--
+    function EmpDelete(config, callBack){
+        var config = config;
+        var callback = callBack;
+        var depID = config.depID;
+        var empID = config.empID;
+
+        $.ajax({
+            url: '/empDelete',
+            type: "POST",
+            dataType: 'json',
+            data: {
+                depID: depID,
+                empID: empID
+            },
+            success: function (result) {
+                alert("Delete id:" + result);
+                console.log(result);
+                callBack('empList');
+            },
+            error: function (xhr, resp, text) {
+                console.log(xhr, resp, text);
+            }
+        });
+    }
+//--
+    function employeeEdit(config, callBack){
+        var config = config;
+        var callback = callBack;
+        var depID = config.depID;
+        var empID = config.empID;
+
+        $.ajax({
+            url: '/employeeEdit',
+            type: "GET",
+            dataType: 'json',
+            data: {
+                depID: depID,
+                empID: empID
+            },
+            success: function (data) {
+                alert("Edit id:" + data.id);
+                $('#id').val(data.id);
+                $('#firstName').val(data.firstName);
+                $('#secondName').val(data.secondName);
+                $('#grade').val(data.grade);
+                $('#birthday').val(data.birthday);
+                $('#eMail').val(data.eMail);
+            },
+            error: function (xhr, resp, text) {
+                console.log(xhr, resp, text);
+            }
+        });
+
+        var firstParent = $('<form id="empSaveForm" method="post" action="empSave"></form>');
+        var row = $('<fildset></fildset>');
+        row.append(' <legend>Employees form </legend>');
+        row.append('<p> <label for="firstName">FirstName </label>' +
+            '<input id="firstName" name="firstName" type="text"> ' +
+            '</p>');
+        row.append('<p> <label for="secondName">LastName </label>' +
+            '<input id="secondName" name="secondName" type="text"> ' +
+            '</p>');
+        row.append('<p> <label for="grade">Gade   </label>' +
+            '<input id="grade" name="grade" type="number"> ' +
+            '</p>');
+        row.append('<p> <label for="birthday">Birthday </label>' +
+            '<input id="birthday" name="birthday" type="date"> ' +
+            '</p>');
+        row.append('<p> <label for="eMail">eMail </label>' +
+            '<input id="eMail" name="eMail" type="email"> ' +
+            '</p>');
+        row.append('<p> <input id="submit" class="submit" type="submit" value="Submit">' +
+            '</p>');
+        row.append('<input id="id" type="hidden" name="id" value=""/>' +
+            '<input id="depID" type="hidden" name="depID" value=""/>');
+
+        firstParent.append(row);
+        $('#content').append(firstParent);
+
+        $('#depID').val(depID);
+
+        $("body").on("submit", "#empSaveForm", function () {
+            $.ajax({
+                url: '/empSave',
+                type: 'post',
+                dataType: 'json',
+                data: $('#empSaveForm').serialize(),
+                success: function (data) {
+                    console.log(data);
+                    alert('Employee save');
+                    callBack('empList');
+                },
+                error: function (xhr, resp, text) {
+                    console.log(xhr, resp, text);
+                    alert('Employee not save');
+                }
+            });
+        });
+
+    }
 
 }
 
+
+/*
+ validateDepartment() {
+ $('#departmentForm').validate({
+ rules: {
+ name: {
+ required: true,
+ minlength: 5,
+ maxlength: 10,
+ remote: {
+ url: "/uniqueName",
+ type: "POST",
+ data: {
+ id: () => {
+ return $('#id').val();
+ },
+ name: () => {
+ return $('#name').val();
+ }
+ }
+ }
+ }
+ },
+ messages: {
+ name: {
+ required: "Type name, please",
+ minlength: "Your password must be at least 5 characters long",
+ maxlength: "Your password must not be longer than 10 characters",
+ remote: "This name is already used!"
+ }
+ },
+ submitHandler: () => {
+ $( "#saveDepartment" ).addClass('listener').trigger( 'click' );
+ }
+ });
+ };
+
+ validateEmployee() {
+ $('#employeeForm').validate({
+ rules: {
+ name: {
+ required: true,
+ minlength: 5,
+ maxlength: 10
+ },
+ email: {
+ required: true,
+ email: true,
+ remote: {
+ url: "/uniqueEmail",
+ type: "POST",
+ data: {
+ id: () => {
+ return $('#id').val();
+ },
+ email: () => {
+ return $('#email').val();
+ }
+ }
+ }
+ },
+ salary: {
+ required: true,
+ digits: true,
+ },
+ birthDate: {
+ required: true,
+ date: true
+ }
+ },
+ messages: {
+ name: {
+ required: "Type name, please",
+ minlength: "Your password must be at least 5 characters long",
+ maxlength: "Your password must not be longer than 10 characters",
+ },
+ email: {
+ required: "Type email, please",
+ email: "Type correct email!!",
+ remote: "This email is already used!"
+ },
+ salary: {
+ required: "Input salary, please",
+ digits: "Type only digits!",
+ },
+ birthDate: {
+ required: "Type birthday, please",
+ date: "input correct date"
+ }
+ },
+ submitHandler: () => {
+ $("#saveEmployee").addClass('listener').trigger('click');
+ }
+ });
+ };
+ */
 
 
 
